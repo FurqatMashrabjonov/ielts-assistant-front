f2979890463a1563178d71b29f0f4e9031d85b84
FurqatMashrabjonov <php_lesson@mail.ru>
2023-05-16 22:03:47 +0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div id="app" style="margin-top: 10px">
+        <van-nav-bar
+                title="Title"
+                left-text="Back"
+                right-text="Button"
+                left-arrow
+                @click-left="onClickLeft"
+                @click-right="onClickRight"
+        />
+        <van-popup v-model="show" round position="bottom"  :style="{ height: height + '%' }" >
+            <h6 style="margin: 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque dolores eaque illo quam. Amet ducimus eaque natus perspiciatis quia quo recusandae repellendus veniam. Accusantium dolore eius rerum ut voluptatibus.</h6>
+            <van-divider />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi delectus deleniti harum illo nam praesentium quasi reprehenderit. Aut doloremque ea esse ex molestias officiis omnis pariatur possimus tempore ut?
+        </van-popup>
+    </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+<script>
+import {Toast} from 'vant';
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+    data() {
+        return {
+            show: false,
+            height: 45
+        }
+    },
+    created() {
 
-    &.router-link-exact-active {
-      color: #42b983;
+    },
+    methods: {
+        onClickLeft() {
+            Toast('Back');
+        },
+        onClickRight() {
+            Toast('Button');
+            this.show = true
+        },
     }
-  }
 }
-</style>
+</script>
+
